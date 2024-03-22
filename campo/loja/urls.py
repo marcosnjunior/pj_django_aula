49c900afd11821be0378d7a03002aeeecd75fe17
marcosnjunior <marcosnjunior@gmail.com>
2024-03-22 18:18:19 +0000

    criando rotas

---
+++ campo/loja/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('pesquisar',views.pesquisar, name='pesquisar'),
+    path('cadastrar',views.cadastrar, name='cadastrar'),
+]
---
+++ campo/loja/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def pesquisar(request):
+    print("Opa Chegou aqui")
+
+
+def cadastrar(request):
+    print("Chegou no cadastrar")
